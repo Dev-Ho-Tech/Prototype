85ddf65040f37153290ef29940c23f7ad5b020d2
Coboy16 <smautrap.16@gmail.com>
2025-03-18 10:15:41 -0500

    update dasboard

--- src/modules/employees/incidents/IncidentsScreen.tsx
+++ src/modules/employees/incidents/IncidentsScreen.tsx
@@ -383,7 +383,7 @@ const IncidenciasScreen: React.FC = () => {
                 </div>
                 <input
                   type="text"
-                  placeholder="Buscar por ID, Cédula, Nombre, Sede, Departamento..."
+                  placeholder="Buscar por código, Cédula, Género, Tipo de Trabajo, Departamento, Sección, Unidad..."
                   className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
--- src/modules/scheduling/components/SearchFilterComponent.tsx
+++ src/modules/scheduling/components/SearchFilterComponent.tsx
@@ -56,7 +56,7 @@ const SearchFilterComponent: React.FC<SearchFilterComponentProps> = ({
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
           <input
             type="text"
-            placeholder="Buscar por ID, Cédula, Nombre, Sede, Departamento..."
+            placeholder="Buscar por código, Cédula, Género, Tipo de Trabajo, Departamento, Sección, Unidad..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             className="w-full pl-10 pr-36 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
